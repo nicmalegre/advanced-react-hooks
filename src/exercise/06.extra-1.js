d9d06217224d087eb0d64420af99ea0cda3234f9
Nicolás Alegre <nikoalegre@hotmail.com>
2022-10-05 12:42:57 -0300

    Complete exercise 6

--- src/exercise/06.extra-1.js
+++ src/exercise/06.extra-1.js
@@ -3,10 +3,12 @@
 
 import * as React from 'react'
 
+const format = ({query, state}) => `\`${query}\` => ${state}`
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
 
-  React.useDebugValue(`\`${query}\` => ${state}`)
+  React.useDebugValue({query, state}, format)
 
   React.useEffect(() => {
     let mounted = true
